6630c7f046b5c984d786f064573b62db15683f2f
matikurcze00 <m.roszkowski00@gmail.com>
2021-03-22 03:42:56 +0100

    mamy tooo

---
+++ changed_keys_anrolab1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import os
+from glob import glob
+package_name = 'changed_keys_anrolab1'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*_launch.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='dominika',
+    maintainer_email='domio626@gmail.com',
+    description='Używanie klawiszy g-góra, d-dół, p-prawo, l- lewo do sterowania zolwiem',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+                'talker = changed_keys_anrolab1.movement_publisher:main',
+        ],
+    },
+)
