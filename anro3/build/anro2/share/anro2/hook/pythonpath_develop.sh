c8328a5b337b667abd70bf0f07e1642574755cd3
matikurcze00 <m.roszkowski00@gmail.com>
2021-04-24 22:51:04 +0200

    pierwsze koty za ploty

---
+++ anro3/build/anro2/share/anro2/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/matikurcze00/dev_ws/src/roszkowski_zajac/anro2/build/anro2"
---
+++ anro3/setup.py
@@ -0,0 +1,33 @@
+import os
+from glob import glob
+from setuptools import setup
+from setuptools import find_packages
+
+package_name = 'anro3'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.py')),
+        (os.path.join('share', package_name), glob('urdf/*')),
+        (os.path.join('share', package_name), glob('anro3')),
+        (os.path.join('share', package_name), glob('*.json')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='dominika',
+    maintainer_email='domio626@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'nonkdl= anro3.nonkdl_dkin:main'
+        ],
+    },
+)
